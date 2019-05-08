77e017b2227fbf2da0e61e966a56671e2c1cb293
Rav018 <R.vanjarapu@diyarme.com>
2019-05-08 11:37:51 +0300

    second commit

---
+++ sample2.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Class1
+
+End Class<html xmlns="www.w3.org/1999/xhtml">
+<head runat = "server" >
+    <title></title>
+</head>
+<body>
+<form id = "form1" runat="server">
+	<div>
+
+<%Response. Write( "HeIIo World") %>
+
+	</div>
+	</form>
+</body>
+</html>
+<html xmlns = "www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title></title>
+</head>
+<body>
+<form id = "form1" runat="server">
+	<div>
+
+<%Response. Write( "HeIIo World") %>
+
+	</div>
+	</form>
+</body>
+</html>
