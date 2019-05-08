0a88e3dd1794184df40870d89902c7dd8941af87
Rav018 <49653790+Rav018@users.noreply.github.com>
2019-05-08 12:58:16 +0300

    TEST-7
    COMMITING IN BRANCH LEVEL

--- sample2.vb
+++ sample2.vb
@@ -30,3 +30,44 @@ End Class<html xmlns="www.w3.org/1999/xhtml">
 	</form>
 </body>
 </html>
+<html xmlns="www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+</head>
+<body>
+	<form id="form1" runat="server">
+	<div>
+
+	<%Response. Write( "HeIIo World") %>
+
+	</div>
+	</form>
+</body>
+</html><html xmlns="www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+</head>
+<body>
+	<form id="form1" runat="server">
+	<div>
+
+	<%Response. Write( "HeIIo World") %>
+
+	</div>
+	</form>
+</body>
+</html>
+<html xmlns="www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+</head>
+<body>
+	<form id="form1" runat="server">
+	<div>
+
+	<%Response. Write( "HeIIo World") %>
+
+	</div>
+	</form>
+</body>
+</html>
